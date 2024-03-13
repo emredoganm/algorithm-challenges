6fda84310599aa661266389b671b4408294fa603
Emre Dogan <emredoganm@live.com>
2024-03-13 11:51:12 +0300

    add remove zero sum sublist implementation

--- src/remove-zero-sum-sublists/index.ts
+++ src/remove-zero-sum-sublists/index.ts
@@ -1,8 +1,46 @@
-interface ListNode {
+export class ListNode {
   val: number;
   next: ListNode | null;
+
+  constructor(val: number = 0, next: ListNode | null = null) {
+    this.val = val;
+    this.next = next;
+  }
 }
 
-function removeZeroSumSublists(head: ListNode | null): ListNode | null {
-  console.log(head.next);
+export default function removeZeroSumSublists(head: ListNode | null): ListNode | null {
+  let startingNode = head;
+  let headToReturn = head;
+
+  while (startingNode) {
+    let endingNode = startingNode.next;
+    let runningSum = startingNode.val;
+
+    while (endingNode) {
+      runningSum += endingNode.val;
+
+      if (runningSum === 0) {
+        if (headToReturn === startingNode) {
+          headToReturn = endingNode.next;
+        } else {
+          let current: ListNode | null = headToReturn;
+
+          while (current) {
+            if (current.next === startingNode) {
+              current.next = endingNode.next;
+              break;
+            }
+
+            current = current.next;
+          }
+        }
+      }
+
+      endingNode = endingNode.next;
+    }
+
+    startingNode = startingNode.next;
+  }
+
+  return headToReturn;
 }
---
+++ src/remove-zero-sum-sublists/remove-zero-sum-sublists.test.ts
@@ -0,0 +1,52 @@
+import removeZeroSumSublists, { ListNode } from ".";
+
+describe("Remove Zero Sum Sublists", () => {
+  it("should work for example#1", () => {
+    const result = removeZeroSumSublists(createLinkedList([1, 2, -3, 3, 1]));
+    expect(deserialize(result)).toStrictEqual([3, 1]);
+  });
+
+  it("should work for example#2", () => {
+    const result = removeZeroSumSublists(createLinkedList([1, 2, 3, -3, 4]));
+    expect(deserialize(result)).toStrictEqual([1, 2, 4]);
+  });
+
+  it("should work for example#3", () => {
+    const result = removeZeroSumSublists(createLinkedList([1, 2, 3, -3, -2]));
+    expect(deserialize(result)).toStrictEqual([1]);
+  });
+
+  it("should work for example#4", () => {
+    const result = removeZeroSumSublists(createLinkedList([4, 40, -45, 1, 2, 4, 40, -40, 32]));
+    expect(deserialize(result)).toStrictEqual([2, 4, 32]);
+  });
+
+  it("should remove full list and return null", () => {
+    const result = removeZeroSumSublists(createLinkedList([1, 2, -3]));
+    expect(deserialize(result)).toStrictEqual(null);
+  });
+});
+
+const createLinkedList = (values: number[]): ListNode => {
+  const list = values.map((value) => new ListNode(value));
+
+  list.forEach((node, index) => {
+    node.next = list[index + 1] || null;
+  });
+
+  return list[0]; // returning the head of the linked list
+};
+
+const deserialize = (head: ListNode | null): number[] | null => {
+  if (head === null) return null;
+
+  let current: ListNode | null = head;
+  const result: number[] = [];
+
+  while (current) {
+    result.push(current.val);
+    current = current.next;
+  }
+
+  return result;
+};
